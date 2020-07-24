52fdf88a7e04a703f4bcff2b2da105ad0ac46a3b
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-07-24 19:57:44 +0530

    single pose for posenet

--- Blazor.Extensions.ML5/ML5/PoseNet/PoseNet.cs
+++ Blazor.Extensions.ML5/ML5/PoseNet/PoseNet.cs
@@ -3,6 +3,7 @@ using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ML5
 {
@@ -43,6 +44,14 @@ namespace ML5
             await runtime.InvokeVoidAsync("poseNetML5", Hash.ToString(), DotNetObjectRef, element, options, type);
 
         }
+        public async Task<string> OnSinglePose(ElementReference? canvasOrVideoOrImage=null)
+        {
+            if(canvasOrVideoOrImage!=null)
+            return await Runtime.InvokeAsync<string>("poseNetsinglePoseML5", Hash.ToString(), canvasOrVideoOrImage);
+            else
+                return await Runtime.InvokeAsync<string>("poseNetsinglePoseML5", Hash.ToString(), null);
+
+        }
         ~PoseNet()
         {
             destroy();
@@ -52,6 +61,19 @@ namespace ML5
             await Runtime.InvokeVoidAsync("destroyPoseNetML5", Hash.ToString());
 
         }
+
+
+
+
+        [JSInvokable("PNFML")]
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task __ModelLoadedPN__()
+        {
+            OnModelLoad?.Invoke();
+        }
+
+        public delegate void ModelLoadedHandler();
+        public event ModelLoadedHandler OnModelLoad;
 
         public enum DetectionType
         {
--- Blazor.Extensions.ML5/wwwroot/ml5PoseNet.js
+++ Blazor.Extensions.ML5/wwwroot/ml5PoseNet.js
@@ -18,6 +18,9 @@ function poseNetML5(hash, dotnet, video = null, options = null, type = null)
         if (options != null) {
             poseNet = ml5.poseNet(poseNetModelLoad.bind(dotnet), options);
         }
+        else {
+            poseNet = ml5.poseNet(poseNetModelLoad.bind(dotnet));
+        }
     }
     PoseNets[hash] = poseNet;
 }
@@ -25,5 +28,11 @@ function destroyPoseNetML5(hash) {
     delete PoseNets[hash];
 }
 function poseNetModelLoad() {
-    console.log("Loaded model");
+    this.invokeMethodAsync("PNFML", "__ModelLoadedPN__");
+}
+
+async function poseNetsinglePoseML5(hash, canvas) {
+    var results = await PoseNets[hash].singlePose(canvas);
+    console.log(results);
+    return JSON.stringify(results);
 }
