e2990458d35360b9f7f55c0c3ed3931c539fff64
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-01-24 14:53:12 +0530

    cleanup

---
+++ BlazorML5/ML5.cs
@@ -0,0 +1,14 @@
+﻿using BlazorBindGen;
+using Microsoft.JSInterop;
+
+namespace BlazorML5;
+
+public static class ML5
+{
+    internal const string CdnMl5 = "https://unpkg.com/ml5@latest/dist/ml5.min.js";
+    public static async ValueTask InitAsync(IJSRuntime runtime)
+    {
+        await BindGen.InitAsync(runtime);
+        await BindGen.ImportAsync(CdnMl5);
+    }
+}
